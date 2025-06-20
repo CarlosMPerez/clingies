bf7c4ab0ba11c703a2a59367793af5995c1268ba
Carlos <CarlosMPerez>
2025-06-20 19:41:41 +0200

    WIP: Initial Avalonia + migrations + tray + ClingyWindow skeleton

---
+++ Clingies.App/Windows/ClingyWindow.axml.cs
@@ -0,0 +1,28 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Clingies.App.Windows;
+
+public partial class ClingyWindow : Window
+{
+    public ClingyWindow()
+    {
+        InitializeComponent();
+        AttachDragEvents();
+    }
+
+    private void OnClose(object? sender, RoutedEventArgs e)
+    {
+        // TODO : SOFT DELETE FROM DB
+        this.Close();
+    }
+
+    private void AttachDragEvents()
+    {
+        PointerPressed += (_, e) =>
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+                BeginMoveDrag(e);
+        };
+    }    
+}
